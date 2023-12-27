11cb8fece288aeba57a2227a069f466d5d8a3e33
Aman <152907353+saltykovpavelter@users.noreply.github.com>
2023-12-27 21:13:02 +0100

    Create silver-octo.swift

---
+++ silver-octo.swift
@@ -0,0 +1,7 @@
+struct StrokeTextLabel: UIViewRepresentable {
+    
+    let font: UIFont
+    let text: String
+    let strokeWidth: CGFloat
+    let textColot: UIColor
+    let strokeColor: UIColor
